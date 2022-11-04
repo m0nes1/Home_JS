b5628a40b07c8bbbfe741974a4ec225c97c88301
Valentina <105123172+m0nes1@users.noreply.github.com>
2022-11-04 17:50:04 +1300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+
+let arr = [1,2, false, 'hell', 24, 'world', undefined, null, 'error', 22,]
+
+
+     
+arr_string = [],
+   arr_num = [],
+     
+let = arr.filter(item => {
+    if (typeof item === 'number' && 'number' >= 5 ) {
+
+        console.log('good');
+       
+  }  else {
+    console.log('bad');
+   
+    }
+   
+});
+
+
+
+
+
+
+
+
+
+// let type = arr.filter (num => num ===  )
+
+//  arr.filter (numb => {
+//      if (numb === typeof number >=5 ) {
+//          number.push(numb) }
+//          else {
+//             str.push(str)
+//          }
+//         })
+         
+    
+        
+   
+
+
+// arr.filter (arr => {
+//    if )
+// })
+
+
+
+// if (arr === number && number >= 5) {
+//     console.log(good);
+// }
+  
+//  console.log(typeof(arr));
+
+// console.log(arr .includes(number));
+
+    
+
+
+
+2
+ let arr_2 =[{a: {price: 20}}][{a: {price: 35}}] 
+// let total = {a: {price: 20}}, {a: {price: 35}};
+             
+ let total = 20+35+40
+ console.log(total);
+
+    
+
